e248020f91ff532ed91dd62aa38b7264fbd192bd
owen-mcmanus <owen.mcmanus727@gmail.com>
2023-01-09 21:51:21 -0800

    copied over swerve templet

---
+++ src/main/java/frc/robot/Drivebase.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class Drivebase {
+    public static void driveFieldRelative(){
+
+    }
+
+    public static void driveFieldRelative(double rotation){
+        
+    }
+
+    public static void driveFieldRelative(Translation2d translation, double rotation){
+        
+    }
+
+    public static void driveRobotRelative(){
+
+    }
+
+    public static void driveRobotRelative(Translation2d translation, double rotation){
+        
+    }
+}
--- src/main/java/frc/robot/Joystick.java
+++ src/main/java/frc/robot/Joystick.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 public final class Joystick {
 
     public static boolean isAutoAlignActive() {
@@ -8,5 +10,13 @@ public final class Joystick {
 
     public static boolean isAutoBalanceActive(){
         return false;
+    }
+
+    public static double getSwerveRotation(){
+        return 0;
+    }
+
+    public static Translation2d getSwerveTranslation(){
+        return new Translation2d();
     }
 }
