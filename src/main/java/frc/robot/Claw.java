6b6dbddcbb6f9d6c84bbe4127b97765aab24fe9a
overlordethan <117047607+overlordethan@users.noreply.github.com>
2023-01-10 20:32:51 -0800

    Minimal progress
    kinda stuck, but I did get the joystick done but in the wrong way

--- src/main/java/frc/robot/Claw.java
+++ src/main/java/frc/robot/Claw.java
@@ -1,12 +1,27 @@
 package frc.robot;
+<<<<<<< Updated upstream
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+=======
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DriverStation;
+>>>>>>> Stashed changes
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class Claw {
     TalonSRX motorOne = new TalonSRX(1);
     TalonSRX motorTwo = new TalonSRX(2);
+<<<<<<< Updated upstream
     JoystickButton buttonOne = new JoystickButton(null, 0)
     buttonOne.getRawButton(5, 1);
+=======
+    JoystickButton buttonOne = new JoystickButton(null, 0);
+}
+
+public boolean getRawButton(int button) {
+    
+>>>>>>> Stashed changes
 }
