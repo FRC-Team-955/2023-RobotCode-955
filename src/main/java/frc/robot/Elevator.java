d696543a4d5695a3f3617fecebc92bdc5d696487
NytePhoenix <np2020.ax@gmail.com>
2023-01-15 15:35:50 -0800

    small adjustments

--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -3,6 +3,7 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 
 public class Elevator {
@@ -10,10 +11,11 @@ public class Elevator {
     PIDController pid;
 
     public Elevator() {
-       elevatorMotor = new CANSparkMax(Constants.ElevatorConstants.kElevatorMotorId, MotorType.kBrushless);
-       pid = new PIDController(Constants.ElevatorConstants.kPElevator, 
+        elevatorMotor = new CANSparkMax(Constants.ElevatorConstants.kElevatorMotorId, MotorType.kBrushless);
+        pid = new PIDController(Constants.ElevatorConstants.kPElevator, 
                                 Constants.ElevatorConstants.kIElevator, 
                                 Constants.ElevatorConstants.kDElevator);
+        ElevatorFeedforward feedforward = new ElevatorFeedforward(0, 0, 0);
     }
     
     public void moveElevator(double joyPos) {
@@ -29,21 +31,21 @@ public class Elevator {
         double elevatorSetpoint = 0;
 
         switch(level) {
-            case 1:
+            case 0:
                 elevatorSetpoint = Constants.ElevatorConstants.kRetracted;
                 break;
-            case 2:
+            case 1:
                 elevatorSetpoint = Constants.ElevatorConstants.kBottomLevel;
                 break;
-            case 3:
+            case 2:
                 elevatorSetpoint = Constants.ElevatorConstants.kMediumLevel;
                 break;
-            case 4:
+            case 3:
                 elevatorSetpoint = Constants.ElevatorConstants.kTopLevel;
                 break;
         }
 
-        double amount = MathUtil.clamp(pid.calculate(elevatorMotor.getEncoder().getPosition(), elevatorSetpoint), -1, 1);
-        elevatorMotor.set(amount);
+        double amount = MathUtil.clamp(pid.calculate(elevatorMotor.getEncoder().getPosition(), elevatorSetpoint), -12, 12);
+        elevatorMotor.setVoltage(amount);
     }
 }
