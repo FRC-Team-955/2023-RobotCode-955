bfb04ea1cc32d206a7c10bdf96df727b97f7404b
kaiyamashita08 <kaiyamashita08@gmail.com>
2023-01-14 20:27:35 -0800

    made major changes
    Ex. the ability to implement multiple actions at the same time

--- src/main/java/frc/robot/AutoStates.java
+++ src/main/java/frc/robot/AutoStates.java
@@ -2,11 +2,12 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Actions.AutoAction;
 import frc.robot.DummyClasses.SwerveDrive;
 
 public class AutoStates {
     SwerveDrive swerve;
-    Enums.State autoState = Enums.State.STOP;
+    Enums.State[] autoStateList = new Enums.State[]{Enums.State.STOP};
     AutoProfileEnum autoProfile;
     private double storedTime;
 
@@ -19,42 +20,47 @@ public class AutoStates {
     }
 
     public void nextState() {
-        autoState = autoProfile.AutoState.get(0);
+        autoStateList = autoProfile.AutoState.get(0);
         autoProfile.AutoState.remove(0);
         storedTime = System.currentTimeMillis();
     }
 
     public void autoPeriodic() {
-        switch (autoState) {
-            case LOADTRAJECTORY:
-                System.out.println("Loading");
-                swerve.loadTrajectory("trajectory");
-                nextState();
-            case CURVE:
-                System.out.println("moving");
-                if(swerve.followTrajectory(180)){
+        for (int i = 0; i < autoStateList.length; i++) {
+            switch (autoStateList[i]) {
+                case LOADTRAJECTORY:
+                    System.out.println("Loading");
+                    swerve.loadTrajectory("trajectory");
                     nextState();
-                }
-            case ACTION:
-                System.out.println("action");
-                if (autoProfile.AutoActions.get(0).Act(autoProfile.AutoActionOptions.get(0))) {
-                    autoProfile.AutoActions.remove(0);
-                    autoProfile.AutoActionOptions.remove(0);
+                case CURVE:
+                    System.out.println("moving");
+                    if(swerve.followTrajectory(180)){
+                        nextState();
+                    }
+                case ACTION:
+                    System.out.println("action");
+                    int[][] storedOption = autoProfile.AutoActionOptions.get(0);
+                    AutoAction[] storedAction = autoProfile.AutoActions.get(0);
+                    if (storedAction[0].Act(storedOption[0])) {
+                        autoProfile.AutoActions.remove(0);
+                        autoProfile.AutoActionOptions.remove(0);
+                        nextState();
+                    }
+                case TRANSITION:
+                    System.out.println("transition");
+                    // not sure whether we'll need this based on autoalign, ect.
                     nextState();
-                }
-            case TRANSITION:
-                System.out.println("transition");
-                // not sure whether we'll need this based on autoalign, ect.
-                nextState();
-            case PAUSE:
-                System.out.println("pause");
-                if (System.currentTimeMillis() - storedTime > autoProfile.AutoPauseTime.get(0)) {
-                    autoProfile.AutoPauseTime.remove(0);
-                    nextState();
-                }
-            case STOP:
-                // set motors to zero, ect.
-                System.out.println("stop");
+                case PAUSE:
+                    System.out.println("pause");
+                    Double[] storedPauseTime = autoProfile.AutoPauseTime.get(0);
+                    if (System.currentTimeMillis() - storedTime > storedPauseTime[0]) {
+                        autoProfile.AutoPauseTime.remove(0);
+                        nextState();
+                    }
+                case STOP:
+                    // set motors to zero, ect.
+                    System.out.println("stop");
+            }
         }
     }
 }
