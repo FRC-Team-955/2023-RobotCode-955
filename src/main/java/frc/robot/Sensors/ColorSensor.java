b5a0a30edefdd9c9536b049a5b545c7a248f60b0
NytePhoenix <np2020.ax@gmail.com>
2023-01-15 16:08:15 -0800

    can sense both colors

--- src/main/java/frc/robot/Sensors/ColorSensor.java
+++ src/main/java/frc/robot/Sensors/ColorSensor.java
@@ -7,31 +7,32 @@ import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class ColorSensor {
-    // not sensing yellow right now for some reason (might need to rescan the cube)
     ColorSensorV3 colorSensor;
     ColorMatch colorMatch;
     Color cube = new Color(0.211181640625, 0.317626953125, 0.471435546875);
+    Color cone = new Color(0.3955078125, 0.51025390625, 0.0947265625); // not actually the cone
 
     public ColorSensor() {
         colorSensor = new ColorSensorV3(Port.kOnboard);
         colorMatch = new ColorMatch();
         colorMatch.addColorMatch(cube); 
-        colorMatch.addColorMatch(Color.kYellow); // need to check rgb values for cone
+        colorMatch.addColorMatch(cone); // need to check rgb values for cone
     }
 
-    // Note from Owen: return the detected color ("p" or "y" as a string)
-    public void senseColor() {
+    public String senseColor() {
         System.out.println("r" + colorSensor.getColor().red); // values from 0 to 1
         System.out.println("g" + colorSensor.getColor().green);
         System.out.println("b" + colorSensor.getColor().blue);
         if(colorMatch.matchClosestColor(colorSensor.getColor()).color == cube) {
-            System.out.println("violet");
+            System.out.println("p");
+            return "p";
         }
-        else if(colorMatch.matchClosestColor(colorSensor.getColor()).color == Color.kYellow) {
-            System.out.println("yellow");
+        else if(colorMatch.matchClosestColor(colorSensor.getColor()).color == cone) {
+            System.out.println("y");
+            return "y";
         }
         else {
-            System.out.println(" ");
+            return "y";
         }
     }
 
