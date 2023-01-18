973ed33f90b50089e36a6846016a997062edab75
NytePhoenix <np2020.ax@gmail.com>
2023-01-17 20:13:00 -0800

    added feedforward and manual override

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,8 @@
 package frc.robot;
 
+
 public final class Constants {
-    
+   
     public final static class ElevatorConstants {
         // Elevator Motor Id's
         public static int kElevatorMotorId = 0;
@@ -17,7 +18,10 @@ public final class Constants {
         public static double kPElevator = 1;
         public static double kIElevator = 0;
         public static double kDElevator = 0;
+        // Elevator Feedforward Values
+        public static double kSElevator = 1;
+        public static double kGElevator = 0;
+        public static double kVElevator = 0;
     }
-    
+   
 }
-
--- src/main/java/frc/robot/Elevator.java
+++ src/main/java/frc/robot/Elevator.java
@@ -1,51 +1,69 @@
 package frc.robot;
 
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 
+
 public class Elevator {
-    CANSparkMax elevatorMotor; 
+    CANSparkMax elevatorMotor;
     PIDController pid;
+    ElevatorFeedforward feedforward;
+
 
     public Elevator() {
         elevatorMotor = new CANSparkMax(Constants.ElevatorConstants.kElevatorMotorId, MotorType.kBrushless);
-        pid = new PIDController(Constants.ElevatorConstants.kPElevator, 
-                                Constants.ElevatorConstants.kIElevator, 
+        pid = new PIDController(Constants.ElevatorConstants.kPElevator,
+                                Constants.ElevatorConstants.kIElevator,
                                 Constants.ElevatorConstants.kDElevator);
-        ElevatorFeedforward feedforward = new ElevatorFeedforward(0, 0, 0);
+        feedforward = new ElevatorFeedforward(Constants.ElevatorConstants.kSElevator,
+                                            Constants.ElevatorConstants.kGElevator,
+                                            Constants.ElevatorConstants.kVElevator);
     }
-    
+   
     public void moveElevator(double joyPos) {
-        if(elevatorMotor.getEncoder().getPosition() >= Constants.ElevatorConstants.kElevatorUpperLimit || 
-            elevatorMotor.getEncoder().getPosition() <= Constants.ElevatorConstants.kElevatorLowerLimit) { // if elevator hit the top or bottom
-            elevatorMotor.set(0);
-        } else {
+        if(Joystick.isOverrideEnabled() == false) {
+            if(elevatorMotor.getEncoder().getPosition() >= Constants.ElevatorConstants.kElevatorUpperLimit ||
+                elevatorMotor.getEncoder().getPosition() <= Constants.ElevatorConstants.kElevatorLowerLimit) { // if elevator hit the top or bottom
+                elevatorMotor.set(0);
+            } else {
+                elevatorMotor.set(joyPos);
+            }
+        }
+        else {
             elevatorMotor.set(joyPos);
         }
     }
 
-    public void setElevator(int level) { // level = desired elevator level
-        double elevatorSetpoint = 0;
 
-        switch(level) {
-            case 0:
-                elevatorSetpoint = Constants.ElevatorConstants.kRetracted;
-                break;
-            case 1:
-                elevatorSetpoint = Constants.ElevatorConstants.kBottomLevel;
-                break;
-            case 2:
-                elevatorSetpoint = Constants.ElevatorConstants.kMediumLevel;
-                break;
-            case 3:
-                elevatorSetpoint = Constants.ElevatorConstants.kTopLevel;
-                break;
+    public void setElevator(int level, double joyPos) { // level = desired elevator level
+        if(Joystick.isOverrideEnabled() == false) {
+            double elevatorSetpoint = 0;
+            switch(level) {
+                case 0:
+                    elevatorSetpoint = Constants.ElevatorConstants.kRetracted;
+                    break;
+                case 1:
+                    elevatorSetpoint = Constants.ElevatorConstants.kBottomLevel;
+                    break;
+                case 2:
+                    elevatorSetpoint = Constants.ElevatorConstants.kMediumLevel;
+                    break;
+                case 3:
+                    elevatorSetpoint = Constants.ElevatorConstants.kTopLevel;
+                    break;
+            }
+
+
+            double amount = MathUtil.clamp(pid.calculate(elevatorMotor.getEncoder().getPosition(), elevatorSetpoint) +
+                                            feedforward.calculate(elevatorMotor.getEncoder().getVelocity()), -12, 12);
+            elevatorMotor.setVoltage(amount);
         }
-
-        double amount = MathUtil.clamp(pid.calculate(elevatorMotor.getEncoder().getPosition(), elevatorSetpoint), -12, 12);
-        elevatorMotor.setVoltage(amount);
+        else {
+            elevatorMotor.set(joyPos);
+        }
     }
 }
