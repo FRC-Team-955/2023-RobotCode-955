98dcb0913fee1fdfce20a72d4fb606095515b268
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2023-01-17 19:45:14 -0800

    added two more motors
    I commented out the PID things since I'm not going to use them. I also added two more motors and added stuff so they spin. I also added constants to the "settings" class. Farris turned the settings class into the constants class btw.

--- src/main/java/frc/robot/Claw.java
+++ src/main/java/frc/robot/Claw.java
@@ -12,30 +12,38 @@ import edu.wpi.first.wpilibj.XboxController;
 public class Claw {
     static TalonSRX motorOne;
     static TalonSRX motorTwo;
+    static TalonSRX motorThree;
+    static TalonSRX motorFour;
     static PIDController pidController;
     //makes the motors and pid controller
     public Claw(){
-        motorOne = new TalonSRX(1);
-        motorTwo = new TalonSRX(2);
-        pidController = new PIDController(0, 0, 0);
+        motorOne = new TalonSRX(Constants.Claw.motorOneNum);
+        motorTwo = new TalonSRX(Constants.Claw.motorTwoNum);
+        motorThree = new TalonSRX(Constants.Claw.motorThreeNum);
+        motorFour = new TalonSRX(Constants.Claw.motorFourNum);
+        // pidController = new PIDController(0, 0, 0); //NOT USED
     }
     
     //sucks in the game piece 
     public static void intakeGamePiece(){
-        motorOne.set(TalonSRXControlMode.PercentOutput, 0.3); // note from owen: add the 0.3 to settings
-        motorTwo.set(TalonSRXControlMode.PercentOutput, -0.3);
+        motorOne.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput); // note from owen: add the 0.3 to settings
+        motorTwo.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);// done
+        motorThree.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput);
+        motorFour.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);
     }
 
     //spits out the game piece
     public static void outputGamePiece(){
-        motorOne.set(TalonSRXControlMode.PercentOutput, -0.3);
-        motorTwo.set(TalonSRXControlMode.PercentOutput, 0.3);
+        motorOne.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);
+        motorTwo.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput);
+        motorThree.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);
+        motorFour.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput);
     }
     
     //PID and stuf
     public static void useIntake(double percentOutput){
-        pidController.setSetpoint(100); // note from owen: why are we using pid here?
-        motorOne.move(MathUtil.clamp(pidController.calculate(motorOne.getSelectedSensorPosition()), -1, 1));
+        // pidController.setSetpoint(100); // note from owen: why are we using pid here? ethan: idk it was abandoned
+        // motorOne.move(MathUtil.clamp(pidController.calculate(motorOne.getSelectedSensorPosition()), -1, 1));
     } 
 
     public static void stopIntake(){
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public static class Constants {
+    public static class Claw{
+        double motorOutput = 0.3;
+        double motorOneNum = 1;
+        double motorTwoNum = 2;
+        double motorThreeNum = 3;
+        double motorFourNum = 4;
+    }
+}
