a7d84034c1f18b9772d0d95c55cca89e9d4a642a
overlordethan <117047607+overlordethan@users.noreply.github.com>
2023-01-19 18:47:32 -0800

    Minor edits, and deletion of unused stuff

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -9,7 +9,7 @@ import edu.wpi.first.math.controller.PIDController;
 // import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class Claw {
+public class Intake {
     static TalonSRX motorOne;
     static TalonSRX motorTwo;
     static TalonSRX motorThree;
@@ -17,7 +17,7 @@ public class Claw {
     static colorSensor proximityDetector;
     // static PIDController pidController;
     //makes the motors and pid controller
-    public Claw(){
+    public Intake(){
         motorOne = new TalonSRX(Constants.Claw.motorOneNum);
         motorTwo = new TalonSRX(Constants.Claw.motorTwoNum);
         motorThree = new TalonSRX(Constants.Claw.motorThreeNum);
@@ -28,6 +28,7 @@ public class Claw {
    
     Timer timer = new Timer();
     public static void startIntake(){
+        timer.reset();
         timer.start();
     }
     //sucks in the game piece and stop
@@ -35,16 +36,11 @@ public class Claw {
         if(timer < 4){
         motorOne.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput); // note from owen: add the 0.3 to settings
         motorTwo.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);// done
-        motorThree.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput);
-        motorFour.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);
 
         colorSensor.senseObj();
         if colorSenor.senseObj == true{
             motorOne.set(TalonSRXControlMode.PercentOutput, 0);
             motorTwo.set(TalonSRXControlMode.PercentOutput, 0);
-            motorThree.set(TalonSRXControlMode.PercentOutput, 0);
-            motorFour.set(TalonSRXControlMode.PercentOutput, 0);
-            timer.reset();
         }
         else{
             
@@ -53,28 +49,8 @@ public class Claw {
 }
 
     //spits out the game piece
-    public static void outputGamePiece(){
+    public static void reverseEthanWheels(){
         motorOne.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);
         motorTwo.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput);
-        motorThree.set(TalonSRXControlMode.PercentOutput, -Constants.Claw.motorOutput);
-        motorFour.set(TalonSRXControlMode.PercentOutput, Constants.Claw.motorOutput);
-    }
-
-    //PID and stuf
-    public static void useIntake(double percentOutput){
-        // pidController.setSetpoint(100); // note from owen: why are we using pid here? ethan: idk it was abandoned
-        // motorOne.move(MathUtil.clamp(pidController.calculate(motorOne.getSelectedSensorPosition()), -1, 1));
-    } 
-
-    public static void stopIntake(){
-
-    }
-
-    public static void useOutput(double percentOutput){
-
-    }
-
-    public static void stopOutput(){
-
     }
 }
