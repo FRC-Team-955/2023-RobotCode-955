713da9cd30a88e8769041b46a5171e63f0a07691
BryanZChen <55962677+BryanZChen@users.noreply.github.com>
2023-02-10 18:31:25 -0800

    Got rid of a space

--- src/main/java/frc/robot/Sensors/Gyro.java
+++ src/main/java/frc/robot/Sensors/Gyro.java
@@ -1,6 +1,6 @@
 package frc.robot.Sensors;
 
-import com.ctre.phoenix.sensors.Pigeon2; 
+import com.ctre.phoenix.sensors.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.MathUtil; 
 
