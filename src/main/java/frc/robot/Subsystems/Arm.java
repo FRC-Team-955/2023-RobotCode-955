e936625516a085373fd40d510efd22f40ec2698a
Jayden Chen <115948034+JaydenCh3n@users.noreply.github.com>
2023-02-21 19:05:18 -0800

    encoder ratio fixed part 2

--- src/main/java/frc/robot/Subsystems/Arm.java
+++ src/main/java/frc/robot/Subsystems/Arm.java
@@ -65,7 +65,7 @@ public final class Arm {
         encoderLog = new DoubleLogEntry(log, "/arm/encoder");
     }
     public static double getOffsetPosition(){
-        return encoder.getPosition() / (80 * (2/3));
+        return (encoder.getPosition() / (80 * (2/3)) + 50);
     }
     public static void logData() {
         motorLog.append(motor.get());
