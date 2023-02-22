bab34e2ecdad2c6dcdf7275a6d70f05c1876193d
jjxfan <114874647+kaiyamashita08@users.noreply.github.com>
2023-02-21 20:14:10 -0800

    started changes

--- src/main/java/frc/robot/Subsystems/IntakeV2.java
+++ src/main/java/frc/robot/Subsystems/IntakeV2.java
@@ -8,13 +8,16 @@ import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.util.datalog.DataLog;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.I2C.Port;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorSensorV3;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxAbsoluteEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -23,20 +26,23 @@ import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
 public class IntakeV2 {
     private static CANSparkMax retractMotor = new CANSparkMax(Constants.IntakeV2.retractMotorID, MotorType.kBrushless);
     private static TalonSRX handOffMotor = new TalonSRX(Constants.IntakeV2.retractMotorID);
+    private static PIDController pid = new PIDController(0, 0, 0);
+    private static RelativeEncoder encoder;
 
     public static void setup() {
         retractMotor.setIdleMode(IdleMode.kBrake);
+        encoder = retractMotor.getEncoder();
+        pid.setTolerance(Constants.IntakeV2.tolerance);
+    }
+
+    private static double getMovement(double setpoint) {
+        return pid.calculate(setpoint) + Math.cos(encoder.getPosition() * 2 * Math.PI) * Constants.IntakeV2.feedforwardConstant;
     }
 
     public static boolean extend() {
         if (!IO.isOverrideEnabled()) {
-            if (retractMotor.getOutputCurrent() < Constants.IntakeV2.intakeAmpThreshhold) {
-                retractMotor.set(Constants.IntakeV2.retractMotorRun);
-            }
-            else {
-                retractMotor.set(0);
-                return true;
-            }
+            retractMotor.setVoltage(MathUtil.clamp(getMovement(Constants.IntakeV2.extendPosition), -12, 12));
+            return pid.atSetpoint();
         }
         return false;
     }
@@ -44,25 +50,20 @@ public class IntakeV2 {
 
     public static boolean retract() {
         if (!IO.isOverrideEnabled()) {
-            if (retractMotor.getOutputCurrent() < Constants.IntakeV2.intakeAmpThreshhold) {
-                retractMotor.set(-Constants.IntakeV2.retractMotorRun);
-            }
-            else {
-                retractMotor.set(0);
-                return true;
-            }
+            retractMotor.setVoltage(MathUtil.clamp(getMovement(Constants.IntakeV2.retractPosition), -12, 12));
+            return pid.atSetpoint();
         }
         return false;
     }
 
     public static boolean intake() {
         if (!IO.isOverrideEnabled()) {
-            handOffMotor.set(TalonSRXControlMode.PercentOutput, Constants.IntakeV2.handOffMotorRun);
             if (handOffMotor.getStatorCurrent() >= Constants.IntakeV2.intakeAmpThreshhold) {
                 handOffMotor.set(TalonSRXControlMode.PercentOutput, 0);
                 return true;
             }
             else {
+                handOffMotor.set(TalonSRXControlMode.PercentOutput, Constants.IntakeV2.handOffMotorRun);
                 return false;
             }
         }
