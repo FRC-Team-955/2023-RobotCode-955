fce8199e4fd73625f8ef5ccae1fba30efe31f2e6
richard4583 <97572875+richard4583@users.noreply.github.com>
2023-03-24 10:13:35 -0800

    auto settings

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -285,16 +285,16 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     autoTypeSelection = AutoType.Old;
-    autoLeaveSelection = AutoLeaveSelection.Right;
-    newAutoLeaveSelection = NewAutoLeaveSelection.Right;
-    autoGridSelection = 6;
-    autoState = AutoState.Done;
+    autoLeaveSelection = AutoLeaveSelection.Charge;
+    newAutoLeaveSelection = NewAutoLeaveSelection.Left;
+    autoGridSelection = 5;
+    autoState = AutoState.Setup; //AutoState.Setup for games
     gamePiecePositionArray = new GamePiecePosition[]{
-      new GamePiecePosition(0,IO.GridRowPosition.High),
-      new GamePiecePosition(1,IO.GridRowPosition.High)
+      new GamePiecePosition(8,IO.GridRowPosition.High),
+      new GamePiecePosition(7,IO.GridRowPosition.High)
       // new GamePiecePosition(7,IO.GridRowPosition.Retract)
     };
-    newAutoState = NewAutoState.Done;
+    newAutoState = NewAutoState.Setup;
     numberOfGamepieces = gamePiecePositionArray.length;
     numberOfGamepiecesPlaced = 0;
     isAutoConeNodePosition = true;
